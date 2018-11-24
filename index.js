ae1b8d42600b3813d6f8e7d804e3073e6ad135cb
Andrew <aedgar777@gmail.com>
2018-11-24 17:26:14 -0500

    Added new message info fetching for notifications

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
- const functions = require('firebase-functions');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.sendNotification = functions.firestore.document('/dm_threads/{thread_id}/messages/{message_id}')
+    .onCreate((snapshot, context) => {
+
+
+        const newMessage = snapshot.data();
+
+        const senderName = newMessage.authorName;
+        const senderID = newMessage.authorUID;
+        const messageText = newMessage.message;
+        const recipientName = newMessage.recipientName;
+        const recipientID = newMessage.recipientUID;
+        var timestamp = new Date();
+        timestamp = newMessage.timestamp;
+
+        console.log(senderName + " " + senderID + " " + messageText + " " + recipientName + " " + recipientID + " " + timestamp.toString())
+
+
+    });
+
+
