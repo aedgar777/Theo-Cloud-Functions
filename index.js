2a1ae56cd9443e2db5820495c899e4053ced6068
Andrew <aedgar777@gmail.com>
2018-11-25 18:10:14 -0500

    Moving along

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-exports.sendNotification = functions.firestore.document('/dm_threads/{thread_id}/messages/{message_id}')
+exports.sendDMNotification = functions.firestore.document('/dm_threads/{thread_id}/messages/{message_id}')
     .onCreate((snapshot, context) => {
 
 
@@ -14,10 +14,33 @@ exports.sendNotification = functions.firestore.document('/dm_threads/{thread_id}
         const messageText = newMessage.message;
         const recipientName = newMessage.recipientName;
         const recipientID = newMessage.recipientUID;
-        var timestamp = new Date();
-        timestamp = newMessage.timestamp;
+        const timestamp = newMessage.timestamp.toString();
 
         console.log(senderName + " " + senderID + " " + messageText + " " + recipientName + " " + recipientID + " " + timestamp.toString())
+
+        const deviceToken = admin.firestore.document(`/Users/${recipientID}/deviceToken`).once('value');
+
+        return deviceToken.then(result => {
+
+            const token_id = result.val();
+
+
+            const payload = {
+                notification: {
+                    title: senderName,
+                    body: messageText,
+                    icon: "default"
+                }
+            };
+
+            return admin.messaging().sendToDevice(token_id, payload).then(response => {
+
+                console.log('Notification Feature')
+
+            });
+
+
+        });
 
 
     });
