e660d2dc612e867a44a744c5312a7be8d9126e6c
Andrew <aedgar777@gmail.com>
2018-12-09 14:44:31 -0500

    Debugging

--- index.js
+++ index.js
@@ -14,22 +14,27 @@ exports.sendDMNotification = functions.firestore.document('/dm_threads/{thread_i
         const messageText = newMessage.message;
         const recipientName = newMessage.recipientName;
         const recipientID = newMessage.recipientUID;
-        const timestamp = newMessage.timestamp.toString();
+        const timestamp = newMessage.timestamp;
 
-        console.log(senderName + " " + senderID + " " + messageText + " " + recipientName + " " + recipientID + " " + timestamp.toString())
+        console.log(senderName + " " + senderID + " " + messageText + " " + recipientName + " " + recipientID + " " + timestamp.toString());
 
-        let deviceTokenQuery = admin.firestore().collection(`/Users/${recipientID}/device_tokens`);
+        let deviceTokenQuery = admin.firestore().collection(`/Users/${recipientID}/device_tokens/`);
 
         return deviceTokenQuery.get().then(querySnapshot => {
-            console.log('deviceTokenQuery returned')
+            console.log('deviceTokenQuery returned');
 
             let tokens = querySnapshot.docs;
 
+            console.log('1');
 
-            const notificationPromises =tokens.map(token => {
 
-                let token_id = token;
+            const notificationPromises = tokens.map(token => {
+                console.log('2');
 
+                let token_id = token.tokenID.toString();
+
+                console.log(token_id);
+                console.log('3');
 
                 const payload = {
                     notification: {
@@ -40,13 +45,10 @@ exports.sendDMNotification = functions.firestore.document('/dm_threads/{thread_i
                 };
 
 
-                return admin.messaging().sendToDevice(token_id, payload).then(response => {
+                return admin.messaging().sendToDevice(token_id, payload)
 
-                    return console.log('Notification sent to device with ID: ' + token_id)
-
-                });
-
-            })
+            });
+            console.log('4')
             return Promise.all(notificationPromises);
 
         });
