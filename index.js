abf62de47ea341a1bc66e4f667682c865888b0fa
Andrew <aedgar777@gmail.com>
2018-12-20 13:59:40 -0500

    Added retrieval of notification preferences

--- index.js
+++ index.js
@@ -12,78 +12,82 @@ exports.sendDMNotification = functions.firestore.document('/dm_threads/{thread_i
         const senderName = newMessage.authorName;
         const senderID = newMessage.authorUID;
         const messageText = newMessage.message;
-        const recipientName = newMessage.recipientName;
         const recipientID = newMessage.recipientUID;
-        const timestamp = newMessage.timestamp;
         var notificationsOn = null;
-
-        console.log(senderName + " " + senderID + " " + messageText + " " + recipientName + " " + recipientID + " " + timestamp.toString());
-
-        let deviceTokenQuery = admin.firestore().collection(`/users/${recipientID}/device_tokens/`);
 
         var idsToBeSorted = [senderID, recipientID];
         idsToBeSorted.sort();
-        var threadID = idsToBeSorted[0]+idsToBeSorted[1];
+        var threadID = idsToBeSorted[0] + idsToBeSorted[1];
+
+
+        console.log(recipientID);
         console.log(threadID);
-        let recipientThread = functions.firestore.document(`users/${recipientID}/threads/${threadID}`);
-
-        recipientThread
-            .onUpdate((snapshot, context) => {
-            const threadDetails = snapshot.data();
-
-            notificationsOn = threadDetails.notificationsOn;
-
-        });
-
-        if (notificationsOn !== false) {
-
-            return deviceTokenQuery.get().then(querySnapshot => {
-
-                let tokenShapshot = querySnapshot.docs;
-
-                const notificationPromises = tokenShapshot.map(doc => {
 
 
-                    let token_id = doc.data().tokenID;
+        return admin.firestore().collection(`users/${recipientID}/threads/`).doc(`${threadID}`).get().then(queryResult => {
 
-                    console.log("token_id: " + token_id);
-
-
-                    const payload = {
-                        notification: {
-                            title: senderName,
-                            body: messageText,
-                        },
-                        data: {
-
-                            senderID: senderID,
-                            senderName: senderName
-
-                        }
-
-                    };
+            let notificationsOn = queryResult.data().notificationsOn;
+            console.log(notificationsOn);
 
 
-                    return admin.messaging().sendToDevice(token_id, payload).then(response => {
+            if (notificationsOn !== false) {
 
-                        console.log("Notification sent: ", response);
-                    })
-                        .catch(error => {
+                return admin.firestore().collection(`/users/${recipientID}/device_tokens/`).get().then(querySnapshot => {
 
-                            console.log("Error sending message: ", error);
+                    let tokenShapshot = querySnapshot.docs;
 
-                        });
+                    const notificationPromises = tokenShapshot.map(doc => {
+
+
+                        let token_id = doc.data().tokenID;
+
+
+                        const payload = {
+
+                            data: {
+
+                                title: senderName,
+                                body: messageText,
+                                senderID: senderID,
+                                senderName: senderName
+
+                            }
+
+                        };
+
+
+                        return admin.messaging().sendToDevice(token_id, payload).then(response => {
+
+                            console.log("Notification sent: ", response);
+                        })
+                            .catch(error => {
+
+                                console.log("Error sending message: ", error);
+
+                            });
+
+
+                    });
+
+                    return Promise.all(notificationPromises);
 
 
                 });
 
-                return Promise.all(notificationPromises);
 
-            });
+            }
 
-        }
+            return;
+
+
+        });
 
     });
+
+
+
+
+
 
 
 
